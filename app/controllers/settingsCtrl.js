bef03c63f00917266cda4b2a7ecb0b6386ce3588
Radu Gaspar <radu.gaspar@evozon.com>
2016-02-26 15:21:55 +0200

    added partial settings actions

--- app/controllers/settingsCtrl.js
+++ app/controllers/settingsCtrl.js
@@ -9,5 +9,18 @@
 
   function SettingsCtrl($scope) {
     console.log('Hello from SettingsCtrl!');
+    $scope.otherTracker = {};
+    $scope.trackers = [
+      { url: 'http://kat.cr/usearch/', isDefault: true },
+      { url: 'http://filelist.ro/browse.php?search=' }
+    ];
+    $scope.addTracker = function(model) {
+      if(!model) { return; }
+      $scope.trackers.push({ url: model });
+      $scope.otherTracker.url = '';
+    };
+    $scope.deleteTracker = function(tracker) {
+      $scope.trackers.splice($scope.trackers.indexOf(tracker), 1);
+    };
   }
 }());
