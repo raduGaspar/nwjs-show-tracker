83c4547a0b722fee0b9e555ffb3da0ed4350d3f3
Radu Gaspar <radu.bogdan.gaspar@gmail.com>
2016-02-27 11:49:39 +0200

    language service refactoring

--- app/services/L.js
+++ app/services/L.js
@@ -1,56 +1,58 @@
-/*global app:false */
-'use strict';
+(function() {
+  'use strict';
 
-angular
-  .app
-  .factory('L', ['$locale', '$http',
-    function($locale, $http) {
-      var i18n, promise,
-        locales = ['en', 'ro'],
-        loadLocale = function(locale) {
-          locale = locale || $locale.id;
-          locale = locale.toLowerCase();
+  angular
+    .app
+    .factory('L', ['$locale', '$http',
+      function($locale, $http) {
+        // TODO: refactor this to read the i18n folder for lang files
+        var i18n, promise,
+          locales = ['en', 'ro'],
+          loadLocale = function(locale) {
+            locale = locale || $locale.id;
+            locale = locale.toLowerCase();
 
-          var allowedLocale = locales.indexOf(locale) > -1,
-            useLocale = allowedLocale ? locale : locales[0];
+            var allowedLocale = locales.indexOf(locale) > -1,
+              useLocale = allowedLocale ? locale : locales[0];
 
-          promise = $http.get('i18n/language-' + useLocale + '.json')
-            .then(function(res) {
-              i18n = res.data;
-            });
-        };
+            promise = $http.get('i18n/lang-' + useLocale + '.json')
+              .then(function(res) {
+                i18n = res.data;
+              });
+          };
 
-      // load locale json
-      if(!i18n) {
-        loadLocale(locales[0]);
-      } else {
-        console.log('locale json already loaded');
-      }
-
-      function dotSelector(obj, str) {
-        var path = str.split('.'),
-          pathLen = path.length,
-          i = 0,
-          key;
-        for (i; i < pathLen; i++) {
-          var key = path[i];
-          if (key in obj) {
-            obj = obj[key];
-          } else {
-            return;
-          }
+        // load locale json
+        if(!i18n) {
+          loadLocale(locales[0]);
+        } else {
+          console.log('locale json already loaded');
         }
-        return obj;
-      }
 
-      function translate(key, args) {
-        return dotSelector(i18n, key);
-      }
+        function dotSelector(obj, str) {
+          var path = str.split('.'),
+            pathLen = path.length,
+            i = 0,
+            key;
+          for (i; i < pathLen; i++) {
+            var key = path[i];
+            if (key in obj) {
+              obj = obj[key];
+            } else {
+              return;
+            }
+          }
+          return obj;
+        }
 
-      return {
-        promise: promise,
-        translate: translate,
-        loadLocale: loadLocale
-      };
-    }
-  ]);
+        function translate(key, args) {
+          return dotSelector(i18n, key);
+        }
+
+        return {
+          promise: promise,
+          translate: translate,
+          loadLocale: loadLocale
+        };
+      }
+    ]);
+}());
