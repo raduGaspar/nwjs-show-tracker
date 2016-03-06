bfa44c70a79c8020b098beb4ebe6590a154c3338
Radu Gaspar <radu.bogdan.gaspar@gmail.com>
2016-03-06 20:04:04 +0200

    binded settings ctrl to settings service

--- app/controllers/settingsCtrl.js
+++ app/controllers/settingsCtrl.js
@@ -5,79 +5,23 @@
     .app
     .controller('SettingsCtrl', SettingsCtrl);
 
-  SettingsCtrl.$inject = ['$scope', 'Utils', 'DB', 'SettingsServ'];
+  SettingsCtrl.$inject = ['$scope', 'SettingsServ'];
 
-  function SettingsCtrl($scope, Utils, DB, SettingsServ) {
+  function SettingsCtrl($scope, SettingsServ) {
     console.log('Hello from SettingsCtrl!');
-    var globals = Utils.getGlobals(),
-      dirname = globals.dirname,
-      db = DB.getDb('settings'),
-      toggleSelection = function(tracker) {
-        console.log('toggleSelection', tracker);
-        if(tracker) {
-          Utils.store('tracker', tracker._id);
-          $scope.selected.tracker = Utils.findById($scope.trackers, tracker._id);
-        } else {
-          var selectedTracker = Utils.store('tracker');
-          if(!selectedTracker) {
-            console.log('select first tracker in list');
-            Utils.store('tracker', $scope.trackers[0]._id);
-            $scope.selected.tracker = $scope.trackers[0];
-          } else {
-            console.log('we have a tracker', selectedTracker);
-            $scope.selected.tracker = Utils.findById($scope.trackers, selectedTracker);
-          }
-        }
-      },
-      updateView = function(data) {
-        if(data) {
-          if(data.length !== undefined) {
-            $scope.trackers = data;
-          } else {
-            $scope.trackers.push(data);
-          }
-        }
 
-        toggleSelection();
-
-        if(!$scope.$$phase) {
-          $scope.$apply();
-        }
-      };
-
-    console.log('current tracker', Utils.store('tracker'));
-
-    // find all records and restore defaults if no data is found
-    DB.find(db, {}).then(function(docs) {
-      docs.length ? updateView(docs) : SettingsServ.reset();
-    }, Utils.onError);
-
-    $scope.otherTracker = {
-      url: 'http://'
-    };
-    $scope.selected = {};
+    $scope.trackerName = { url: 'http://' };
     $scope.restoreDefaults = SettingsServ.reset;
-    $scope.toggleSelection = toggleSelection;
     $scope.addTracker = function(url) {
       if(!url) { return; }
-
-      DB.insert(db, { url: url }).then(function(newTracker) {
-        $scope.otherTracker.url = 'http://';
-        updateView(newTracker);
-      }, Utils.onError);
+      $scope.settings.trackers.list.push({ url: url });
+      $scope.trackerName.url = 'http://';
     };
-    $scope.deleteTracker = function(tracker) {
-      console.log('delete tracker', tracker);
-      // TODO:
-      // 1. if you delete a selected tracker, select the first one
-      DB.delete(db, { _id: tracker._id }).then(function(numRemoved) {
-        $scope.trackers.splice($scope.trackers.indexOf(tracker), 1);
-        if(Utils.store('tracker') === tracker._id) {
-          // remove localstorage tracker
-          localStorage.removeItem('tracker');
-        }
-        updateView();
-      }, Utils.onError);
+    $scope.deleteTracker = function(tracker, idx) {
+      $scope.settings.trackers.list.splice(idx, 1);
+      if($scope.settings.trackers.selected > $scope.settings.trackers.list.length - 1) {
+        $scope.settings.trackers.selected = 0;
+      }
     };
   }
 }());
