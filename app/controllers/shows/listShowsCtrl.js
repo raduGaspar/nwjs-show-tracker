d06cd622b31307d0b9c6eebd5536ab60e46c79e0
Radu Gaspar <radu.gaspar@evozon.com>
2016-03-07 11:33:20 +0200

    added show search based on selected tracker

--- app/controllers/shows/listShowsCtrl.js
+++ app/controllers/shows/listShowsCtrl.js
@@ -5,9 +5,9 @@
     .app
     .controller('ListShowsCtrl', ListShowsCtrl);
 
-  ListShowsCtrl.$inject = ['$scope', '$state', '$document', 'Utils', 'DB'];
+  ListShowsCtrl.$inject = ['$scope', '$state', '$document', 'Utils', 'DB', 'SettingsServ'];
 
-  function ListShowsCtrl($scope, $state, $document, Utils, DB) {
+  function ListShowsCtrl($scope, $state, $document, Utils, DB, SettingsServ) {
     console.log('Hello from ListShowsCtrl!');
     /*
      * TODO:
@@ -15,9 +15,11 @@
      * 2. allow shows export/import to/from JSON
      * 3. pagination (maybe lazy loading) & filtering
      */
-    var fileSaveTimeout,
+
+
+    var settings,
       globals = Utils.getGlobals(),
-      db = DB.getDb('shows'),
+      showsDb = DB.getDb('shows'),
       scopeUpdate = function() {
         if(!$scope.$$phase) {
           $scope.$apply();
@@ -32,78 +34,80 @@
         return show.onlyEps ? ep : 's' + se + 'e' + ep;
       },
       doUpdate = function(show) {
-        DB.update(db, { _id: show._id }, show).then(function(data) {
+        DB.update(showsDb, { _id: show._id }, show).then(function(data) {
           console.log(data);
+        }, Utils.onError);
+      },
+      init = function() {
+        settings = SettingsServ.get();
+
+        $scope.showData = { name: undefined };
+        $scope.getEpisodeToView = getEpisodeToView;
+        $scope.selectedShow;
+
+        $scope.selectShow = function(show) {
+          $scope.selectedShow = show;
+        };
+
+        $scope.addShow = function() {
+          console.log('addShow');
+          $state.go('shows.add');
+          scopeUpdate();
+        };
+
+        $scope.nextEpisode = function(show) {
+          console.log('increase episode', show);
+          var season = show.seasons[show.seasons.length-1];
+          if($scope.pressedKey === 18 && !show.onlyEps) {
+            show.seasons.push({ ep: 1 });
+          } else {
+            season.ep++;
+          }
+
+          doUpdate(show);
+          return false;
+        };
+
+        $scope.prevEpisode = function(show) {
+          console.log('decrease episode', show);
+          var season = show.seasons[show.seasons.length-1];
+          if(season.ep > 1) {
+            season.ep--;
+          } else {
+            if(show.seasons.length > 1) {
+              show.seasons.pop();
+            }
+          }
+
+          doUpdate(show);
+          return false;
+        };
+
+        $scope.searchTorrent = function(show) {
+          var next = getEpisodeToView(show),
+            searchFor = show.name + ' ' + next;
+          console.log(searchFor);
+          globals.gui.Shell.openExternal(
+            settings.trackers.list[settings.trackers.selected].url +
+            encodeURI(searchFor)
+          );
+        };
+
+        $scope.deleteShow = function(show, idx) {
+          DB.delete(showsDb, show).then(function(data) {
+            console.log('deleted show', data, show);
+            $scope.shows.splice(idx, 1);
+          });
+        };
+
+        // get all shows
+        DB.find(showsDb, {}).then(function(docs) {
+          console.log('showsDb shows data', docs);
+          $scope.shows = docs;
+          scopeUpdate();
         }, Utils.onError);
       };
 
-    $scope.showData = {
-      name: undefined
-    };
-
-    $scope.getEpisodeToView = getEpisodeToView;
-    $scope.selectedShow;
-
-    $scope.selectShow = function(show) {
-      $scope.selectedShow = show;
-    };
-
-    $scope.addShow = function() {
-      console.log('addShow');
-      $state.go('shows.add');
-      scopeUpdate();
-    };
-
-    $scope.nextEpisode = function(show) {
-      console.log('increase episode', show);
-      var season = show.seasons[show.seasons.length-1];
-      if($scope.pressedKey === 18 && !show.onlyEps) {
-        show.seasons.push({ ep: 1 });
-      } else {
-        season.ep++;
-      }
-
-      doUpdate(show);
-      return false;
-    };
-
-    $scope.prevEpisode = function(show) {
-      console.log('decrease episode', show);
-      var season = show.seasons[show.seasons.length-1];
-      if(season.ep > 1) {
-        season.ep--;
-      } else {
-        if(show.seasons.length > 1) {
-          show.seasons.pop();
-        }
-      }
-
-      doUpdate(show);
-      return false;
-    };
-
-    $scope.searchTorrent = function(show) {
-      var next = getEpisodeToView(show),
-        searchFor = show.name + ' ' + next;
-      console.log(searchFor);
-      globals.gui.Shell.openExternal(
-        'http://kat.cr/usearch/' +
-        encodeURI(searchFor)
-      );
-    };
-
-    $scope.deleteShow = function(show, idx) {
-      DB.delete(db, show).then(function(data) {
-        console.log('deleted show', data, show);
-        $scope.shows.splice(idx, 1);
-      });
-    };
-
-    // get all shows
-    DB.find(db, {}).then(function(docs) {
-      console.log('db shows data', docs);
-      $scope.shows = docs;
-      scopeUpdate();
-    }, Utils.onError);
+    SettingsServ.promise.then(init, Utils.onError);
   }
 }());
