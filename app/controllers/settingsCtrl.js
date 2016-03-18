23fc85fc00dff3ce9ee4179bd48e4d3b969fa863
Radu Gaspar <radu.gaspar@evozon.com>
2016-03-18 16:09:07 +0200

    removed promised based initialization methods from controllers

--- app/controllers/settingsCtrl.js
+++ app/controllers/settingsCtrl.js
@@ -29,66 +29,60 @@
         }
       });
 
-    var trackers,
-      pre = 'http://',
-      init = function() {
-        $scope.trackerName = { url: pre };
-        $scope.restoreDefaults = SettingsServ.reset;
-        $scope.addTracker = function(url) {
-          if(!url) { return; }
-          trackers.list.push({ url: url });
-          $scope.trackerName.url = pre;
-        };
-        $scope.deleteTracker = function(idx) {
-          console.log(arguments);
-          trackers.list.splice(idx, 1);
-          if(idx <= trackers.selected) {
-            // prevent it from going under 0
-            if(trackers.selected) {
-              trackers.selected--;
-            }
-          }
-        };
-        $scope.doExport = function(input) {
-          if(input.files.length) {
-            DB.export()
-              .then(function(res) {
-                var json = angular.toJson(Utils.cleanIds(res));
-                return Utils.writeFile(input.files[0].path, json);
-              }, Utils.onError)
-              .then(function(file) {
-                console.log('file saved to', file);
-                Utils.onSuccess({
-                  message: L.translate('alerts.dataExport', { file: file })
-                });
+    var trackers = SettingsServ.get().trackers,
+      pre = 'http://';
 
-                // required for saving a file with the same name
-                input.value = '';
-              }, Utils.onError);
-          }
-        };
-        $scope.doImport = function(input) {
-          if(input.files.length) {
-            Utils.readFile(input.files[0].path)
-              .then(function(data) {
-                var data = angular.fromJson(data);
-                return DB.import(data);
-              }, Utils.onError)
-              .then(function(res) {
-                console.log('import result', res);
-                // TODO: angular scope needs to be updated with the imported data
-                // awful lazy solution
-                $window.location.reload();
-              }, Utils.onError);
-          }
-        };
-        $scope.languages = L.languages;
-        $scope.changeLanguage = L.loadLocale;
-      };
+    $scope.trackerName = { url: pre };
+    $scope.restoreDefaults = SettingsServ.reset;
+    $scope.addTracker = function(url) {
+      if(!url) { return; }
+      trackers.list.push({ url: url });
+      $scope.trackerName.url = pre;
+    };
+    $scope.deleteTracker = function(idx) {
+      console.log(arguments);
+      trackers.list.splice(idx, 1);
+      if(idx <= trackers.selected) {
+        // prevent it from going under 0
+        if(trackers.selected) {
+          trackers.selected--;
+        }
+      }
+    };
+    $scope.doExport = function(input) {
+      if(input.files.length) {
+        DB.export()
+          .then(function(res) {
+            var json = angular.toJson(Utils.cleanIds(res));
+            return Utils.writeFile(input.files[0].path, json);
+          }, Utils.onError)
+          .then(function(file) {
+            console.log('file saved to', file);
+            Utils.onSuccess({
+              message: L.translate('alerts.dataExport', { file: file })
+            });
 
-    SettingsServ.promise.then(function() {
-      trackers = SettingsServ.get().trackers;
-      init();
-    });
+            // required for saving a file with the same name
+            input.value = '';
+          }, Utils.onError);
+      }
+    };
+    $scope.doImport = function(input) {
+      if(input.files.length) {
+        Utils.readFile(input.files[0].path)
+          .then(function(data) {
+            var data = angular.fromJson(data);
+            return DB.import(data);
+          }, Utils.onError)
+          .then(function(res) {
+            console.log('import result', res);
+            // TODO: angular scope needs to be updated with the imported data
+            // awful lazy solution
+            $window.location.reload();
+          }, Utils.onError);
+      }
+    };
+    $scope.languages = L.languages;
+    $scope.changeLanguage = L.loadLocale;
   }
 }());
--- app/controllers/shows/listShowsCtrl.js
+++ app/controllers/shows/listShowsCtrl.js
@@ -39,7 +39,7 @@
         }
       });
 
-    var settings,
+    var settings = SettingsServ.get(),
       globals = Utils.getGlobals(),
       showsDb = DB.getDb('shows'),
       scopeUpdate = function() {
@@ -62,108 +62,99 @@
           .then(function(res) {
             console.log('show update success', res);
           }, Utils.onError);
-      },
-      init = function() {
-        settings = SettingsServ.get();
-
-        $scope.showData = { name: undefined };
-        $scope.getEpisodeToView = getEpisodeToView;
-
-        $scope.selectShow = function(show) {
-          $scope.selectedShow = show;
-        };
-
-        $scope.showActions = function(show) {
-          var ssa = $scope.activeShowActions;
-          if(ssa && ssa._id === show._id) {
-            $scope.activeShowActions = null;
-          } else {
-            $scope.activeShowActions = show;
-          }
-        };
-
-        $scope.addShow = function() {
-          console.log('addShow');
-          $state.go('main.add');
-          scopeUpdate();
-        };
-
-        $scope.nextEpisode = function(show) {
-          console.log('increase episode', show);
-          var season = show.seasons[show.seasons.length-1];
-          if($scope.pressedKey === 18 && !show.onlyEps) {
-            show.seasons.push({ ep: 1 });
-          } else {
-            season.ep++;
-          }
-
-          doUpdate(show);
-          return false;
-        };
-
-        $scope.prevEpisode = function(show) {
-          console.log('decrease episode', show);
-          var season = show.seasons[show.seasons.length-1];
-          if(season.ep > 1) {
-            season.ep--;
-          } else {
-            if(show.seasons.length > 1) {
-              show.seasons.pop();
-            }
-          }
-
-          doUpdate(show);
-          return false;
-        };
-
-        $scope.searchTorrent = function(show) {
-          var next = getEpisodeToView(show),
-            searchFor = show.name + ' ' + next;
-          console.log(searchFor);
-          globals.gui.Shell.openExternal(
-            settings.trackers.list[settings.trackers.selected].url +
-            encodeURI(searchFor)
-          );
-        };
-
-        $scope.deleteShow = function(show) {
-          DB.delete(showsDb, { _id: show._id }).then(function(data) {
-            console.log('deleted show', data, show);
-            $scope.shows.splice($scope.shows.indexOf(show), 1);
-          });
-        };
-
-        $scope.editShow = function(show) {
-          console.log('edit', show);
-          ShowsServ.setSelected(show);
-          $state.go('main.add');
-        };
-
-        $scope.today = function() {
-          return new Date().getDay();
-        };
-
-        $scope.showTodayOnly = true;
-
-        // get all shows
-        DB.find(showsDb, {}).then(function(docs) {
-          console.log('showsDb shows data', docs);
-          // create a temporary object to hold all translatable values
-          // this will allow filtering by day name based on current translation
-          // it will not be stored in the DB
-          for(var i=0; i<docs.length; i++) {
-            docs[i]['_temp'] = docs[i]['_temp'] || {};
-            docs[i]['_temp'].day = L.translate('weekdays')[docs[i].airsOn];
-          }
-          $scope.shows = docs;
-          scopeUpdate();
-        }, Utils.onError);
       };
 
-    SettingsServ.promise
-      .then(function() {
-        return L.promise;
-      }, Utils.onError)
-      .then(init, Utils.onError);
+    $scope.showData = { name: undefined };
+    $scope.getEpisodeToView = getEpisodeToView;
+
+    $scope.selectShow = function(show) {
+      $scope.selectedShow = show;
+    };
+
+    $scope.showActions = function(show) {
+      var ssa = $scope.activeShowActions;
+      if(ssa && ssa._id === show._id) {
+        $scope.activeShowActions = null;
+      } else {
+        $scope.activeShowActions = show;
+      }
+    };
+
+    $scope.addShow = function() {
+      console.log('addShow');
+      $state.go('main.add');
+      scopeUpdate();
+    };
+
+    $scope.nextEpisode = function(show) {
+      console.log('increase episode', show);
+      var season = show.seasons[show.seasons.length-1];
+      if($scope.pressedKey === 18 && !show.onlyEps) {
+        show.seasons.push({ ep: 1 });
+      } else {
+        season.ep++;
+      }
+
+      doUpdate(show);
+      return false;
+    };
+
+    $scope.prevEpisode = function(show) {
+      console.log('decrease episode', show);
+      var season = show.seasons[show.seasons.length-1];
+      if(season.ep > 1) {
+        season.ep--;
+      } else {
+        if(show.seasons.length > 1) {
+          show.seasons.pop();
+        }
+      }
+
+      doUpdate(show);
+      return false;
+    };
+
+    $scope.searchTorrent = function(show) {
+      var next = getEpisodeToView(show),
+        searchFor = show.name + ' ' + next;
+      console.log(searchFor);
+      globals.gui.Shell.openExternal(
+        settings.trackers.list[settings.trackers.selected].url +
+        encodeURI(searchFor)
+      );
+    };
+
+    $scope.deleteShow = function(show) {
+      DB.delete(showsDb, { _id: show._id }).then(function(data) {
+        console.log('deleted show', data, show);
+        $scope.shows.splice($scope.shows.indexOf(show), 1);
+      });
+    };
+
+    $scope.editShow = function(show) {
+      console.log('edit', show);
+      ShowsServ.setSelected(show);
+      $state.go('main.add');
+    };
+
+    $scope.today = function() {
+      return new Date().getDay();
+    };
+
+    $scope.showTodayOnly = true;
+
+    // get all shows
+    DB.find(showsDb, {}).then(function(docs) {
+      console.log('showsDb shows data', docs);
+      // create a temporary object to hold all translatable values
+      // this will allow filtering by day name based on current translation
+      // it will not be stored in the DB
+      for(var i=0; i<docs.length; i++) {
+        docs[i]['_temp'] = docs[i]['_temp'] || {};
+        docs[i]['_temp'].day = L.translate('weekdays')[docs[i].airsOn];
+      }
+      $scope.shows = docs;
+      scopeUpdate();
+    }, Utils.onError);
   }
 }());
