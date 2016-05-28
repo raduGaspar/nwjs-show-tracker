d736029a050fd80f5cc2fe9f96270988e8f3a393
Radu Gaspar <radu.bogdan.gaspar@gmail.com>
2016-05-28 23:02:42 +0300

    updated packaging logic to generate versioned zip files for all OS exports

--- package.js
+++ package.js
@@ -1,4 +1,7 @@
 var NwBuilder = require('nw-builder'),
+  pjson = require('./package.json'),
+  fs = require('fs'),
+  archiver = require('archiver'),
   opts = {
     files: './dist/**', // use the glob format
     version: '0.12.0',
@@ -13,7 +16,30 @@ nw.on('log', console.log);
 
 // start build
 nw.build().then(function() {
-  console.log('App packaged in', opts.buildDir);
+  for(var i=0; i<opts.platforms.length; i++) {
+    (function(platform) {
+      var zipName = platform + '-v' + pjson.version + '.zip';
+      var dest = opts.buildDir + pjson.name + '/';
+      var source = dest + platform + '/';
+      var output = fs.createWriteStream(dest+zipName);
+      var archive = archiver('zip');
+      console.log('Zipping', source);
+
+      output.on('close', function () {
+        console.log('Generated', dest+zipName);
+      });
+
+      archive.on('error', function(err){
+        throw err;
+      });
+
+      archive.pipe(output);
+      archive.bulk([
+        { expand: true, cwd: source, src: ['**/*'], dest: pjson.name }
+      ]);
+      archive.finalize();
+    }(opts.platforms[i]));
+  }
 }).catch(function(error) {
   console.error(error);
 });
