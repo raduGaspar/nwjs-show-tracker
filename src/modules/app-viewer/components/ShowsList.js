9415d0ef8dcd089977363b6925e594c7a2c68006
Radu Gaspar <radu.bogdan.gaspar@gmail.com>
2017-04-16 18:01:22 +0300

    retrieve all tags test implementation

--- src/modules/app-viewer/components/ShowsList.js
+++ src/modules/app-viewer/components/ShowsList.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
+// TODO: test implementation - add to utility class
+const concatUnique = (...arrs) => [...new Set([].concat(...arrs))];
+
 const ShowsList = (props) => {
   console.log('ShowsList::', props);
   const { shows } = props;
+
   if (shows.length) {
-    return <ul>{ shows.map((show, idx) => (<li key={idx}><p>{show.name}</p></li>)) } </ul>;
+    let allTags = [];
+    const lis = shows.map((show, idx) => {
+
+      // TODO: maybe move/use this data in the Menu class
+      allTags = concatUnique(
+        allTags,
+        (show.tags || []).map(t => t.text),
+      );
+      return (
+        <li key={idx}>
+          <p>{show.name}</p>
+        </li>
+      );
+    });
+
+    console.log('allTags', allTags);
+
+    return <ul>{ lis }</ul>;
   }
 
   return <div>no shows</div>;
